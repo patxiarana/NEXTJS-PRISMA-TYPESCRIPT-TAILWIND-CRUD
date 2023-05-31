53c089167e0090148075aa7b724ebbdc8462ce77
patxi <patxiarana05@gmail.com>
2023-05-31 18:39:29 -0300

    get notes realized, render dosen	 work

--- notes/pages/index.tsx
+++ notes/pages/index.tsx
@@ -1,5 +1,21 @@
 "use client";
+import { GetServerSideProps, NextPage } from 'next';
 import { useState } from 'react'
+import { prisma } from '../lib/prisma';
+
+
+interface Notes {
+  
+  notes:{
+    id : string 
+    title : string 
+    content: string
+  }[]
+
+}
+
+
+
 
 interface FormData {
   title:string , 
@@ -7,7 +23,7 @@ interface FormData {
   id: string ,
 }
 
-export default function Home() {
+ const Home = ({notes}: Notes) => {
 const [form, setForm] = useState<FormData>({title:'' , content:'', id: ''});
 
 
@@ -58,8 +74,40 @@ try {
      onChange={e =>  setForm({...form, content: e.target.value})}
      className='border-2 rounded border-gray-600 p-1'
      />
-<button type='submit' className='bg-blue-500 text-white rounded p-1'>Add +</button>
+<button type='submit' className='bg-blue-500 text-white rounded p-1'>Add +</button>   
         </form>
+     <div className="w-auto min-w-[25%] max-w-min mt-20 mx-auto space-y-6 flex flex-col items-stretch">
+     <ul>
+          {notes.map(note => (
+            <li key={note.id} className="border-b border-gray-600 p-2">
+              <div className="flex justify-between">
+                <div className="flex-1">
+                  <h3 className="font-bold">{note.title}</h3>
+                  <p className="text-sm">{note.content}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+          </div>
        </div>
   )
+}
+
+export default Home ; 
+
+export const getServerSideProps : GetServerSideProps = async function() {
+  const notes = await prisma.note.findMany({
+    select: {
+      title:true,
+      id:true,
+      content:true,
+    }
+  })
+
+  return {
+    props : {
+      notes
+    }
+  }
 }
